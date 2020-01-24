b75904118a40879f49c7bc878fc3b2ea355fb1d5
Jasmine Qin <yijia.qin@alumni.ubc.ca>
2020-01-24 01:04:44 -0800

    update report and eda.R

--- src/eda.R
+++ src/eda.R
@@ -38,23 +38,17 @@ main <- function(data_in, files_to){
   # Summary tip percentage every hour
   hour_summary_stats <- taxi %>%
     group_by(pu_hour) %>%
-    summarise(pu_hour_mean = mean(tip_pct),
-              pu_hour_median = median(tip_pct))
-  
-  # plot hour vs. mean
-  tod1 <- hour_summary_stats %>%
-    ggplot(aes(factor(pu_hour), pu_hour_mean)) +
-    geom_point() +
-    labs(x = "Pick-up hour in a day", y = "Mean tip percentage", title = "Mean Tip Percentage vs. Hour in a Day")
-  
-  # plot hour vs. median
-  tod2 <- hour_summary_stats %>%
-    ggplot(aes(factor(pu_hour), pu_hour_median)) +
-    geom_point() +
-    labs(x = "Pick-up hour in a day", y = "Median tip percentage", title = "Median Tip Percentage vs. Hour in a Day")
+    summarise(pu_hour_mean = mean(tip_pct))
   
   # FIRST GRAPH
-  time_of_day <- grid.arrange(tod1, tod2, ncol = 2)
+  # plot hour vs. mean
+  time_of_day <- hour_summary_stats %>%
+    ggplot(aes(factor(pu_hour), pu_hour_mean)) +
+    geom_point() +
+    labs(x = "Pick-up hour in a day", y = "Mean tip percentage (%)") +
+    theme(axis.title = element_text(size = 18),
+          axis.text.x= element_text(size = 14),
+          axis.text.y= element_text(size = 14))
   
   # SECOND GRAPH
   # mean tip percentage vs. day of a week
@@ -63,38 +57,27 @@ main <- function(data_in, files_to){
     summarise(pu_wday_mean = mean(tip_pct)) %>%
     ggplot(aes(factor(pu_wday), pu_wday_mean)) +
     geom_bar(stat = 'identity') +
-    coord_cartesian(ylim = c(0.12, 0.16)) +
-    labs(x = "Trip on day of a week", y = "Mean tip percentage", title = "Mean Tip Percentage vs. Day of a Week")
+    coord_cartesian(ylim = c(12, 16)) +
+    labs(x = "Trip on day of a week", y = "Mean tip percentage (%)") +
+    theme(axis.title = element_text(size = 18),
+          axis.text.x= element_text(size = 14),
+          axis.text.y= element_text(size = 14))
   
-  # THRID TABLE
-  # time of day group summary stats
-  tod_gp_sample_stats <- taxi %>%
-    group_by(pu_time_of_day_group) %>%
-    summarise(tip_mean = mean(tip_pct))
-  
-  # time of day group tip distribution graph
-  time_dist <- taxi %>%
-    ggplot(aes(tip_pct, pu_time_of_day_group)) +
-    geom_density_ridges(alpha = 0.8, bandwidth = 10) +
-    scale_y_discrete(expand = expand_scale(add = c(0.5, 2))) +
-    labs(x = "Tip Percentage", y = "Pick-up Time of a Day Group", title = "Distribution of Tip Percentage")
-  
-  # time of day group mean tip bar chart
-  time_group <- tod_gp_sample_stats %>%
-    ggplot(aes(factor(pu_time_of_day_group), tip_mean)) +
-    geom_bar(stat = 'identity') +
-    coord_flip(ylim = c(0.12, 0.16)) +
-    labs(y = "Tip Percentage", x = "Pick-up Time of a Day Group", title = "Mean tip percentage")
-  
-  # FOURTH GRAPH
-  time_of_day_group <- grid.arrange(time_dist, time_group, ncol = 2)
-  
+  # THRID GRAPH
+  # Heatmap of weekday group x time of day group on tip percentage
+  heatmap <- taxi %>% ggplot(aes(pu_time_of_day_group, pu_wday_group, fill=tip_pct)) + geom_tile() +
+    labs(x = "", y = "", fill = "Tip percentage (%)") +
+    theme(axis.text.x= element_text(size = 16),
+          axis.text.y= element_text(size = 16),
+          legend.text=element_text(size=14),
+          legend.title=element_text(size=16))
+    
   # weekday vs. weekend summary statistics
   wkday_gp_sample_stat <- taxi %>% 
     group_by(pu_wday_group) %>% 
     summarise(mean_tip = mean(tip_pct))
-  
-  # FIFTH GRAPH
+    
+  # FOURTH GRAPH
   # weekday vs. weekend boxplot
   day_of_week_group <- taxi %>%
     filter(tip_pct <= 0.25 & tip_pct >= 0) %>%
@@ -104,23 +87,10 @@ main <- function(data_in, files_to){
     geom_text(data = wkday_gp_sample_stat, aes(label = round(mean_tip, 4), x = as.factor(pu_wday_group), y = mean_tip-0.03)) +
     labs(x = "", y = "Tip percentage", title = "Boxplot of Tip Percentage with Means")
   
-  # SIXTH TABLE
-  # Sample test for significance
-  # times of a day groups
-  tod_sample_test <- lm(tip_pct ~ pu_time_of_day_group, data = taxi)
-  
-  # SEVENTH TABLE
-  # Heatmap of weekday group x time of day group on tip percentage
-  heatmap <- taxi %>% ggplot(aes(pu_time_of_day_group, pu_wday_group, fill=tip_pct)) + geom_tile() +
-    labs(x = "", y = "", fill = "Tip percentage")
-  
-  ggsave(paste0(files_to, "/time_of_day.png"), time_of_day, width = 12, height = 8)
-  ggsave(paste0(files_to, "/day_of_week.png"), day_of_week, width = 4, height = 4)
-  write_csv(tod_gp_sample_stats, path = paste0(files_to, "/time_group_sample_stats.csv"))
-  ggsave(paste0(files_to, "/time_group.png"), time_of_day_group, width = 12, height = 8)
-  ggsave(paste0(files_to, "/day_group.png"), day_of_week_group, width = 4, height = 4)
-  saveRDS(tod_sample_test, file = paste0(files_to, "/test_model.rds"))
+  ggsave(paste0(files_to, "/time_of_day.png"), time_of_day, width = 10, height = 8)
+  ggsave(paste0(files_to, "/day_of_week.png"), day_of_week, width = 10, height = 8)
   ggsave(paste0(files_to, "/heat_map.png"), heatmap, width = 10, height = 8)
+  ggsave(paste0(files_to, "/day_of_week_group.png"), day_of_week_group, width = 10, height = 8)
 }
 
 # Call main function
